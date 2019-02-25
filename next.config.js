1075f3a28534b8e34efcddfba9f9ce8f6b58a316
Kenneth Auchenberg <kenneth@auchenberg.dk>
2019-02-24 19:08:54 -0800

    Update pages

--- next.config.js
+++ next.config.js
@@ -3,25 +3,7 @@ module.exports = {
     exportPathMap: function () {
         return {
             '/': { page: '/' },
-            '/about': { page: '/about' },
-            '/call-for-recommendations': { page: '/call-for-recommendations' },
-            '/tickets': { page: '/tickets' },
-            '/why-coldfront': { page: '/why-coldfront' },
-            '/speakers': { page: '/speakers' },
-            '/program': { page: '/program', query: { selectedDay: 'day2' } },
-            '/program/day1': { page: '/program', query: { selectedDay: 'day1' } },
-            '/program/day2': { page: '/program', query: { selectedDay: 'day2' } },
-            '/community-workshop-day': { page: '/community-workshop-day' },            
-            '/where-to-stay-and-how-to-get-around': { page: '/where-to-stay-and-how-to-get-around' },
-            '/social': { page: '/social' },
-            '/venue-and-practical': { page: '/venue-and-practical' },
-            '/welcome-coldfronter': { page: '/welcome-coldfronter' },
-            '/diversity-scholarships': { page: '/diversity-scholarships' },
-            '/student': { page: '/student' },
-            '/prefront': { page: '/prefront' },
-            '/roadbook': { page: '/roadbook' },
-            '/social-dinners': { page: '/social-dinners' },
-            '/talks-slides-videos-photos': { page: '/talks-slides-videos-photos' },
+            '/about': { page: '/about' }
         }
     }
 }
